87e9088208e2457232656fffd1883b31ce8fc862
Mostafa Mahdieh <m.mahdieh@gmail.com>
2022-02-04 22:14:40 +0330

    - added more asserations - updated gclef code

--- prioritization_clustering_fp_runner.py
+++ prioritization_clustering_fp_runner.py
@@ -16,19 +16,15 @@ to_version = [26, 133, 65, 106, 26]
 
 for index, project in enumerate(projects):
 #    bug_prediction_data_path = '../WTP-data/' + project + '/xgb.csv'
-    bug_prediction_data_path = 'bug_prediction_data/xgboost_res1/' + project + '.csv'
+#    bug_prediction_data_path = 'bug_prediction_data/xgboost_res1/' + project + '.csv'
+#    bug_prediction_data_path = 'bug_prediction_data/xgb_results_14001108_offline/' + project + '.csv'
+    bug_prediction_data_path = 'bug_prediction_data/cb_results_14001109_online/' + project + '.csv'
     print("Reading " + bug_prediction_data_path)
     bug_prediction_data = pd.read_csv(bug_prediction_data_path)
     print("done.")
     for version_number in range(from_version[index], to_version[index] + 1):
         print("* Version %d" % version_number)
-        run_prioritization_clustering_fp(bug_prediction_data, project, version_number, clustering_agg, cosine_distances, [50, 100, 150, 200, 250, 300, 350, 400, 450, 500],
-                                         [0], 'fp0_cosd_clus50_500.csv',
-                                         ['cosd_fp0'])
-#        run_prioritization_clustering_fp(bug_prediction_data, project, version_number, clustering_agg, manhattan_distances, [50, 100, 150, 200, 250, 300, 350, 400, 450, 500],
-#                                         [0], 'fp0_manh_clus50_500.csv',
-#                                         ['manh_fp0'])
-#        run_prioritization_clustering_fp(bug_prediction_data, project, version_number, clustering_agg, euclidean_distances, [50, 100, 150, 200, 250, 300, 350, 400, 450, 500],
-#                                         [0], 'fp0_eucl_clus50_500.csv',
-#                                         ['eucl_fp0'])
+        run_prioritization_clustering_fp(bug_prediction_data, 'cb_score_online', project, version_number, clustering_agg, euclidean_distances, 'average', [200],
+                                         [0.999], 'eucl_cb_results_14001109_online_c0999_200.csv',
+                                         ['eucl_cb_results_14001109_online_c0999'])
         print()
--- prioritization_std_runner.py
+++ prioritization_std_runner.py
@@ -3,18 +3,20 @@ from prioritization.prioritization_manager import run_standard2_prioritization
 
 projects = ['Chart', 'Closure', 'Lang', 'Math', 'Time']
 from_version = [1, 1, 1, 1, 1]
-to_version = [13, 50, 33, 50, 14]
+to_version = [26, 133, 65, 106, 26]
 
 for index, project in enumerate(projects):
 #    bug_prediction_data_path = '../WTP-data/' + project + '/xgb.csv'
 #    bug_prediction_data_path = 'bug_prediction_data/xgboost_res2/' + project + '.csv'
-    bug_prediction_data_path = 'bug_prediction_data/xgboost_res1/' + project + '.csv'
+#    bug_prediction_data_path = 'bug_prediction_data/xgboost_res1/' + project + '.csv'
+#    bug_prediction_data_path = 'bug_prediction_data/xgb_results_14001108_offline/' + project + '.csv'
+    bug_prediction_data_path = 'bug_prediction_data/cb_results_14001109_online/' + project + '.csv'
     print("Reading " + bug_prediction_data_path)
     bug_prediction_data = pd.read_csv(bug_prediction_data_path)
     print("done.")
     for version_number in range(from_version[index], to_version[index] + 1):
         print("* Version %d" % version_number)
-        run_standard2_prioritization(bug_prediction_data, project, version_number,
-                                         [0.95], 'std2_res1_c95.csv',
-                                         ['res1_c95'])
+        run_standard2_prioritization(bug_prediction_data, 'cb_score_online', project, version_number,
+                                         [0.999], 'std_cb_results_14001109_online_c0999.csv',
+                                         ['std_cb_results_14001109_online_c0999'])
         print()
