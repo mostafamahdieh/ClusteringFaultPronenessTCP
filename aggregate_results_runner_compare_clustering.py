99524dac02812894c67de5bbc31671f38697d6e9
Mostafa Mahdieh <m.mahdieh@gmail.com>
2022-02-13 09:39:37 +0330

    updated aggregate results

--- aggregate_results_runner_compare_clustering.py
+++ aggregate_results_runner_compare_clustering.py
@@ -1,3 +1,3 @@
 from results.aggregate_results import aggregate_results
 
-aggregate_results('fp0_eucl_clus50_500.csv', 'fp0_manh_clus50_500.csv', 'fp0_cosd_clus50_500.csv')
+aggregate_results(['fp0_eucl_clus50_500.csv', 'fp0_manh_clus50_500.csv', 'fp0_cosd_clus50_500.csv'], 'compare_clustering')
