47e3afff6121b8192334f64cfa83c60968d9da2d
Mostafa Mahdieh <m.mahdieh@gmail.com>
2022-02-13 08:34:34 +0330

    renamed plot results

--- aggregate_results_runner_compare_clustering.py
+++ aggregate_results_runner_compare_clustering.py
@@ -1,3 +1,3 @@
-from results import aggregate_results
+from results import aggregate_results_res
 
 aggregate_results('fp0_eucl_clus50_500.csv', 'fp0_manh_clus50_500.csv', 'fp0_cosd_clus50_500.csv')
