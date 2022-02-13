480bb467b35101f2c1a6985c19cbe4a96ed4caf8
Mostafa Mahdieh <m.mahdieh@gmail.com>
2022-02-13 09:49:52 +0330

    aggregation updated

--- aggregate_results_runner_compare_clustering.py
+++ aggregate_results_runner_compare_clustering.py
@@ -1,3 +1,9 @@
 from results.aggregate_results import aggregate_results
 
-aggregate_results(['fp0_eucl_clus50_500.csv', 'fp0_manh_clus50_500.csv', 'fp0_cosd_clus50_500.csv'], 'compare_clustering')
+projects = ['Chart', 'Closure', 'Lang', 'Math', 'Time']
+from_version = [1, 1, 1, 1, 1]
+to_version = [26, 133, 65, 106, 26]
+
+filenames = ['fp0_eucl_clus50_500.csv', 'fp0_manh_clus50_500.csv', 'fp0_cosd_clus50_500.csv']
+results_path = '../WTP-data/aggregate/compare_clustering'
+aggregate_results(filenames, projects, from_version, to_version, results_path)
--- results/aggregate_results.py
+++ results/aggregate_results.py
@@ -6,76 +6,41 @@ import os
 import scipy.stats as stats
 import itertools as it
 
-
-def effect_size(lst1, lst2):
-    return improvement(lst1, lst2)
-
-
-def improvement(x, y):
-    n = len(x)
-    a = 0
-    for i in range(0, n):
-#        print("x",x[i],"y",y[i],"x-y",x[i]-y[i])
-        a = a + (x[i] - y[i])
-    improvement = (a/n)
-    return improvement
-
-
-def read_results(file_names, project, from_version, to_version):
-    first_fail = pd.DataFrame(columns=['version'])
-    apfd = pd.DataFrame(columns=['version'])
-
-    for version_number in range(from_version, to_version):
-        data_path = "../WTP-data/%s/%d" % (project, version_number)
-        results_dict_first_fail = {'version': version_number, 'project': project}
-        results_dict_apfd = {'version': version_number, 'project': project}
-        skipped = False
-
-        for file_name in file_names:
-            file_path = '%s/%s' % (data_path, file_name)
-
-            if os.path.isfile(file_path):
-                print("Reading %s" % file_path)
-                results = pd.read_csv(file_path, delimiter=',')
-
-                for i, row in results.iterrows():
-                    results_dict_first_fail[row['alg']] = row['first_fail'] * 100
-                    results_dict_apfd[row['alg']] = row['apfd']
-            else:
-                print("Skipping %s" % file_path)
-                skipped = True
-
-        if not skipped:
-            first_fail = first_fail.append(results_dict_first_fail, ignore_index=True)
-            apfd = apfd.append(results_dict_apfd, ignore_index=True)
-
-    return first_fail, apfd
-
-
-def aggregate_results(file_names, results_folder):
-    projects = ['Chart', 'Closure', 'Lang', 'Math', 'Time']
-    from_version = [1, 1, 1, 1, 1]
-    to_version = [26, 133, 65, 106, 26]
-
-    results_path = '../WTP-data/aggregate/'+results_folder
+def aggregate_results(file_names, projects, from_version, to_version, results_path):
     try:
         os.stat(results_path)
     except:
-        os.mkdir(results_path)       
+        os.mkdir(results_path)
 
+    first_fail = pd.DataFrame(columns=['version'])
+    apfd = pd.DataFrame(columns=['version'])
     for index, project in enumerate(projects):
-        first_fail, apfd = read_results(file_names,
-                                        project, from_version[index], to_version[index] + 1)
+        for version_number in range(from_version[index], to_version[index] + 1):
+            data_path = "../WTP-data/%s/%d" % (project, version_number)
+            results_dict_first_fail = {'version': version_number, 'project': project}
+            results_dict_apfd = {'version': version_number, 'project': project}
+            skipped = False
 
-        if index == 0:
-            apfd_all = apfd
-            first_fail_all = first_fail
-        else:
-            apfd_all = apfd_all.append(apfd)
-            first_fail_all = first_fail_all.append(first_fail)
+            for file_name in file_names:
+                file_path = '%s/%s' % (data_path, file_name)
 
-    first_fail_all = first_fail_all.reset_index()
-    apfd_all = apfd_all.reset_index()
+                if os.path.isfile(file_path):
+                    print("Reading %s" % file_path)
+                    results = pd.read_csv(file_path, delimiter=',')
 
-    first_fail_all.to_csv(results_path+'/first_fail_all.csv')
-    apfd_all.to_csv(results_path+'/apfd_all.csv')
+                    for i, row in results.iterrows():
+                        results_dict_first_fail[row['alg']] = row['first_fail'] * 100
+                        results_dict_apfd[row['alg']] = row['apfd']
+                else:
+                    print("Skipping %s" % file_path)
+                    skipped = True
+
+            if not skipped:
+                first_fail = first_fail.append(results_dict_first_fail, ignore_index=True)
+                apfd = apfd.append(results_dict_apfd, ignore_index=True)
+
+    first_fail_all = first_fail.reset_index()
+    apfd_all = apfd.reset_index()
+
+    first_fail_all.to_csv(results_path + '/first_fail_all.csv')
+    apfd_all.to_csv(results_path + '/apfd_all.csv')
