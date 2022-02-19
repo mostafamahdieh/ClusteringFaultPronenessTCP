95bc2db0395b4ce3541478d6b9409d4f4393d0e5
Mostafa Mahdieh <m.mahdieh@gmail.com>
2022-02-19 18:42:39 +0330

    added clustering plots

---
+++ compare_clustering_agg3_max_with_fp_results.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from pandas import Categorical
+import numpy as np
+from numpy import std, mean, sqrt
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import itertools as it
+from results.compare_clustering_results import compare_clustering_results
+
+projects = ['Chart', 'Closure', 'Lang', 'Math', 'Time']
+to_version = [21, 128, 60, 101, 22]
+#to_version = [13, 50, 33, 50, 14]
+
+data_path = './aggregate/main_results'
+output_path = './aggregate/compare_clustering_agg2_max_with_fp'
+
+cluster_nums = list(range(50,501,50))
+dist_functions = ['eucl', 'cosd']
+dist_complete_names = ['Euclidean', 'Cosine']
+base_algorithmname = '_agg2_xgb14001115_online_c0999_max_clus'
+
+compare_clustering_results(projects, to_version, data_path, output_path, cluster_nums, dist_functions, dist_complete_names, base_algorithmname, "")
